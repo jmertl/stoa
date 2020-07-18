856e0d9f79bfd27d30c40e7e151137db76a08702
Jakub Mertl <mertljakub@gmail.com>
2020-07-18 19:58:05 +0200

    dragging is working

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -7,8 +7,10 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage>
     with SingleTickerProviderStateMixin {
-  AnimationController animationController;
-  final double maxSlide = 225.0;
+  AnimationController animationController;  static const double maxSlide = 225.0;
+  static const double maxDragStartEdge = maxSlide - 16;
+  static const double minDragStartEdge = 60;
+  bool _canBeDragged = false;
 
   @override
   void initState() {
@@ -19,16 +21,21 @@ class _MainPageState extends State<MainPage>
     );
   }
 
-  void toggle() => animationController.isDismissed
-      ? animationController.forward()
-      : animationController.reverse();
+  void close() => animationController.reverse();
+
+  void open() => animationController.forward();
+
+  void toggleDrawer() => animationController.isCompleted ? close() : open();
 
   @override
   Widget build(BuildContext context) {
     var myDrawer = Container(color: Colors.blue);
     var myChild = Container(color: Colors.yellow);
     return GestureDetector(
-      onTap: toggle,
+      onTap: toggleDrawer,
+      onHorizontalDragStart: _onDragStart,
+      onHorizontalDragUpdate: _onDragUpdate,
+      onHorizontalDragEnd: _onDragEnd,
       child: AnimatedBuilder(
           animation: animationController,
           builder: (context, _) {
@@ -48,5 +55,40 @@ class _MainPageState extends State<MainPage>
             );
           }),
     );
+  }
+
+  void _onDragStart(DragStartDetails details) {
+    bool isDragOpenFromLeft = animationController.isDismissed &&
+        details.globalPosition.dx < minDragStartEdge;
+    bool isDragCloseFromRight = animationController.isCompleted &&
+        details.globalPosition.dx > maxDragStartEdge;
+
+    _canBeDragged = isDragOpenFromLeft || isDragCloseFromRight;
+  }
+
+  void _onDragUpdate(DragUpdateDetails details) {
+    if (_canBeDragged) {
+      double delta = details.primaryDelta / maxSlide;
+      animationController.value += delta;
+    }
+  }
+
+  void _onDragEnd(DragEndDetails details) {
+    //I have no idea what it means, copied from Drawer
+    double _kMinFlingVelocity = 365.0;
+
+    if (animationController.isDismissed || animationController.isCompleted) {
+      return;
+    }
+    if (details.velocity.pixelsPerSecond.dx.abs() >= _kMinFlingVelocity) {
+      double visualVelocity = details.velocity.pixelsPerSecond.dx /
+          MediaQuery.of(context).size.width;
+
+      animationController.fling(velocity: visualVelocity);
+    } else if (animationController.value < 0.5) {
+      close();
+    } else {
+      open();
+    }
   }
 }
