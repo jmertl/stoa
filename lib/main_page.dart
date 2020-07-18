7cbb347b876777b106b159734df3f40d1f8b723b
Jakub Mertl <mertljakub@gmail.com>
2020-07-18 15:41:57 +0200

    basics implemented

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -5,19 +5,48 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  final double maxSlide = 225.0;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 250),
+    );
+  }
+
+  void toggle() => animationController.isDismissed
+      ? animationController.forward()
+      : animationController.reverse();
+
   @override
   Widget build(BuildContext context) {
     var myDrawer = Container(color: Colors.blue);
     var myChild = Container(color: Colors.yellow);
-    return Stack(
-      children: <Widget>[
-        myDrawer,
-        Transform(
-          transform: Matrix4.identity()..scale(0.5),
-          alignment: Alignment.centerLeft,
-          child: myChild,)
-      ],
+    return GestureDetector(
+      onTap: toggle,
+      child: AnimatedBuilder(
+          animation: animationController,
+          builder: (context, _) {
+            double slide = maxSlide * animationController.value;
+            double scale = 1 - (animationController.value * 0.3);
+            return Stack(
+              children: <Widget>[
+                myDrawer,
+                Transform(
+                  transform: Matrix4.identity()
+                    ..translate(slide)
+                    ..scale(scale),
+                  alignment: Alignment.centerLeft,
+                  child: myChild,
+                )
+              ],
+            );
+          }),
     );
   }
 }
