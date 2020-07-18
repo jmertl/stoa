9cb92171fac44f2662238eb14fb5c7e3e1e71b90
Jakub Mertl <mertljakub@gmail.com>
2020-07-18 14:40:22 +0200

    checkpoint

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("White space"),
-      ),
+    var myDrawer = Container(color: Colors.blue);
+    var myChild = Container(color: Colors.yellow);
+    return Stack(
+      children: <Widget>[
+        myDrawer,
+        Transform(
+          transform: Matrix4.identity()..scale(0.5),
+          alignment: Alignment.centerLeft,
+          child: myChild,)
+      ],
     );
   }
 }
